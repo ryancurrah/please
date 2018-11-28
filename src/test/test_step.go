61f03a5db96f7ccc25a92b677bfcec538fee8ebc
Ryan Currah <ryan.currah@cantire.com>
2018-11-27 21:34:11 -0500

    fix debug line with env

--- src/test/test_step.go
+++ src/test/test_step.go
@@ -318,7 +318,7 @@ func testCommandAndEnv(state *core.BuildState, target *core.BuildTarget) (string
 
 func runTest(state *core.BuildState, target *core.BuildTarget) ([]byte, error) {
 	replacedCmd, env := testCommandAndEnv(state, target)
-	log.Debug("Running test %s\nENVIRONMENT:\n%s\n%s", target.Label, strings.Join(env, "\n"), replacedCmd)
+	log.Debug("Running test %s\nENVIRONMENT:\n%s\n%s", target.Label, env, replacedCmd)
 	_, stderr, err := core.ExecWithTimeoutShellStdStreams(state, target, target.TestDir(), env, target.TestTimeout, state.Config.Test.Timeout, state.ShowAllOutput, replacedCmd, target.TestSandbox, state.DebugTests, "testing")
 	return stderr, err
 }
